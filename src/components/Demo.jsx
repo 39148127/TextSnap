819597e1b1c72305abdb61379c0066d63dd24926
LilianKwamboka <kwambokalilian59@gmail.com>
2024-03-12 00:36:04 +0300

    browser history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,32 +1,47 @@
-import { useState, useEffect } from "react" // part of the logic for the demo component
-
-//useEffect is a hook that allows us to perform side effects in function components
-//usestate is a hook that allows us to use state in function components
-//react h
+import { useState, useEffect } from "react" 
 
 import { copy, linkIcon, loader, tick} from '../assets'
-import { useLazyGetSummaryQuery} from '../services/article'
+import { useLazyGetSummaryQuery } from '../services/article'
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
-  }); // state for the articles
-  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    )
+
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const {data} = await getSummary({articleUrl: article.url
-    });
 
-    if (data?.summary){
-      const newArticle = {
-        ...article, summary: data.summary
-      };
+    const { data } = await getSummary({ articleUrl: article.
+      url });
 
-      setArticle(newArticle)
-      console.log(newArticle)
-    }
+      if(data?.summary){
+        const newArticle = { ...article, summary: data.
+          summary };
+          const updatedArticles = [newArticle, ...allArticles];
+          
+        setArticle(newArticle);
+        setAllArticles(updatedArticles); // Update the list of articles
+
+        // Copy the summary to the clipboard  do thissssss
+
+        localStorage.setItem('articles', JSON.stringify
+        (updatedArticles));
+      }
   }
   return (
     <section className="mt-16 w-full max-w-xl" >
@@ -34,7 +49,7 @@ const Demo = () => {
       <div className="flex flex-col w-full gap-2">
         <form 
           className="relative flex justify-center items-center "
-          onSubmit={handleSubmit}                                                                                                                                                               Submit={() => {}}
+          onSubmit={handleSubmit}
           >
 
             <img
@@ -55,18 +70,38 @@ const Demo = () => {
             type="submit"
             className="submit_btn"
             >
-              ↵
+              <p>↵</p>
             </button>
-
+            </form>
             {/* Browse history */}
-
-            <div>
+            <div className="flex flex-col gap-1 max-h-60 overflow-auto">
+              {allArticles.map((item, index) => (
+                <div
+                key={`link-${index}`} 
+                onClick={() => setArticle(item)}
+                className="link_card"
+                >
+                  <div className="copy_btn">
+                    <img 
+                     src={copy}
+                     alt="copy_icon"
+                     className="w-[40%] h-[40%] object-contain"
+                    />
+                    </div>
+                    
+                    <p className="flex-1 font-satoshi text-blue-700 
+                    font-medium text-sm truncate">
+                      {item.url}
+                    </p>
+                  </div>
+              ))}
+            </div>
+          <div>
               {/*Output of the summary*/}
             </div>
-        </form>
-      </div>
+          </div>
     </section>
   )
 }
 
-export default Demo;
+export default Demo
